70aa4fd34740f3a0c001138e232c883927102983
realfishbread <yoonh12288@gmail.com>
2024-09-22 19:41:01 +0900

    Initial commit

---
+++ eclipse-workspace/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Spring Boot 애플리케이션임을 나타내는 어노테이션
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);  // 애플리케이션 시작
+    }
+}
+
---
+++ eclipse-workspace/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+    
+  
+    
+}

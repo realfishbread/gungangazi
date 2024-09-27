b33021ad186c31ec2c8b8aa46428571aca693613
CyaN-09D0EF <skawltn3122113@gmail.com>
2024-09-28 00:54:16 +0900

    Add files via upload

--- lib/splash/SplashPage.dart
+++ lib/splash/SplashPage.dart
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-import 'package:example3/main.dart';
-
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 애니메이션 컨트롤러 초기화
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-    _controller.forward();
-
-    // 3초 후에 HomePage로 이동
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => MyHomePage()), // HomePage로 이동
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FadeTransition(
-          opacity: _animation,
-          child: Icon(Icons.pets, size: 100, color: Colors.yellow[200]), // 로고
-        ),
-      ),
-    );
-  }
-}
-=======
 import 'package:example4/main.dart';
 import 'package:flutter/material.dart';
 
@@ -106,4 +52,3 @@ class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateM
     );
   }
 }
->>>>>>> b53280c (첫 커밋 메시지)

162128ac89e0209a948f02c58b6bdc0109530c5e
bback1235 <pjh62870312@gmail.com>
2024-09-28 20:23:37 +0900

    LoginPage.dart

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,113 +1,335 @@
-import 'package:example4/main.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase 초기화
+  await KakaoSdk.init(nativeAppKey: 'YOUR_KAKAO_APP_KEY'); // 카카오 SDK 초기화
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '건강아지',
+      title: 'Login and Sign Up',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow, // 사용자가 원하는 앱 색상
       ),
-      initialRoute: '/',  // 초기 경로 설정
-      routes: {
-        '/': (context) => const LoginPage(),  // 로그인 페이지
-        '/home': (context) => const MyHomePage(),  // 메인 페이지로 이동할 경로 설정
-      },
-      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
 
-// 로그인 페이지
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  String? _loginError; // 로그인 오류 메시지
 
-  // 가짜 데이터베이스 역할 (아이디와 비밀번호 저장)
-  final String validMemberId = 'testUser';
-  final String validPassword = 'password123';
+  Future<void> _googleSignIn() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
-  bool _loginFailed = false; // 로그인 실패 여부
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        await FirebaseAuth.instance.signInWithCredential(credential);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
+    } catch (e) {
+      setState(() {
+        _loginError = '구글 로그인에 실패하였습니다.';
+      });
+    }
+  }
+
+  Future<void> _kakaoSignIn() async {
+    try {
+      final token = await AuthApi.instance.loginWithKakaoTalk();
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: token.accessToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      setState(() {
+        _loginError = '카카오 로그인에 실패하였습니다.';
+      });
+    }
+  }
+
+  Future<void> _login() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // 로그인 성공 시 홈 페이지로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      setState(() {
+        _loginError = '로그인에 실패하였습니다. 이메일 또는 비밀번호를 확인하세요.'; // 오류 메시지 설정
+      });
+    }
+  }
+
+  void _goToSignUp() {
+    // 회원가입 버튼을 누르면 SignUpPage로 이동
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpPage()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('로그인 페이지'),
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 4, // 그림자 효과
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8), // 둥근 모서리
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '로그인',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: '아이디 (Email)'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(labelText: '비밀번호'),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 8),
+                  // 로그인 오류 메시지 표시
+                  if (_loginError != null)
+                    Text(
+                      _loginError!,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: Text('로그인'),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _googleSignIn, // 구글 로그인 버튼
+                    child: Text('구글로 로그인'),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _kakaoSignIn, // 카카오 로그인 버튼
+                    child: Text('카카오로 로그인'),
+                  ),
+                  SizedBox(height: 16),
+                  TextButton(
+                    onPressed: _goToSignUp, // 회원가입 버튼
+                    child: Text('계정이 없으신가요? 회원가입'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  String _selectedGender = 'Male'; // 기본 성별
+
+  void _signUp() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+    String email = _emailController.text;
+    String name = _nameController.text;
+
+    // 회원가입 성공 메시지 표시
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('환영합니다!'),
+          content: Text('회원가입이 완료되었습니다.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // 메시지 창 닫기
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()), // 로그인 화면으로 이동
+                );
+              },
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // 아이디 입력 필드
             TextField(
-              controller: _idController,
-              decoration: const InputDecoration(
-                labelText: '아이디',
-                border: OutlineInputBorder(),
-              ),
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: '아이디'),
             ),
-            const SizedBox(height: 16),
-            // 비밀번호 입력 필드
+            SizedBox(height: 16),
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: '비밀번호',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true, // 비밀번호 숨기기
+              decoration: InputDecoration(labelText: '비밀번호'),
+              obscureText: true,
             ),
-            const SizedBox(height: 8),
-            // 로그인 실패 메시지 표시
-            if (_loginFailed) // 로그인 실패 시만 메시지 표시
-              const Text(
-                '로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.',
-                style: TextStyle(color: Colors.red),
-              ),
-            const SizedBox(height: 16),
-            // 로그인 버튼
-            ElevatedButton(
-              onPressed: () {
-                String memberId = _idController.text;
-                String password = _passwordController.text;
+            SizedBox(height: 16),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: '이메일'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: '이름'),
+            ),
+            SizedBox(height: 16),
+            DropdownButton<String>(
+              value: _selectedGender,
+              items: <String>['남성', '여성'],
+        );
+      },
+    );
+  }
 
-                // 로그인 검증 로직 (아이디와 비밀번호가 올바른지 확인)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: '아이디'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: '비밀번호'),
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: '이메일'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: '이름'),
+            ),
+            SizedBox(height: 16),
+            DropdownButton<String>(
+              value: _selectedGender,
+              items: <String>['남성', '여성']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
                 setState(() {
-                  if (memberId == validMemberId && password == validPassword) {
-                    _loginFailed = false; // 로그인 성공 시 실패 메시지 숨기기
-                    // 로그인 성공 후 메인 페이지로 이동
-                    Navigator.pushReplacementNamed(context, '/home');
-                  } else {
-                    _loginFailed = true; // 로그인 실패 시 메시지 표시
-                  }
+                  _selectedGender = newValue!;
                 });
               },
-              child: const Text('로그인'),
             ),
-            const SizedBox(height: 16),
-            // 회원가입으로 이동하는 버튼
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignUpPage()),
-                );
-              },
-              child: const Text('회원가입'),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _signUp,
+              child: Text('회원가입'),
             ),
           ],
         ),
@@ -116,21 +338,3 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-
-
-// 회원가입 페이지
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('회원가입 페이지'),
-      ),
-      body: const Center(
-        child: Text('회원가입 페이지 내용'),
-      ),
-    );
-  }
-}

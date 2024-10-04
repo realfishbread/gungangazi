b514727981dafb42e312a124c5c7a33a15c4f6d5
CyaN-09D0EF <skawltn3122113@gmail.com>
2024-10-04 22:24:39 +0900

    Update web_home_page.dart

--- lib/web_home_page.dart
+++ lib/web_home_page.dart
@@ -1,4 +1,4 @@
-import 'package:example4/profile2.dart';
+import 'profile2.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'PopupHandler.dart';

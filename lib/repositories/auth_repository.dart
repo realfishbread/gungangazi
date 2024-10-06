367ccda871c1a7a87a15dd807f18e8e551738c7f
CyaN-09D0EF <skawltn3122113@gmail.com>
2024-10-06 22:42:28 +0900

    move file

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,26 @@
+// repositories/auth_repository.dart
+import 'package:dio/dio.dart';
+import '../dto/login_dto.dart';
+
+class AuthRepository {
+  final Dio _dio = Dio();
+
+  // 로그인 API 호출
+  Future<LoginResponseDto?> login(LoginRequestDto loginRequest) async {
+    try {
+      Response response = await _dio.post(
+        'http://15.164.140.55/api/login',
+        data: loginRequest.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        return LoginResponseDto.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } on DioError catch (e) {
+      print('Login error: ${e.message}');
+      return null;
+    }
+  }
+}

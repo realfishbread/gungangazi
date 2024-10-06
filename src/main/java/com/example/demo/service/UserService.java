1536299e11a4fbe08eec2786e61fa0553924adab
yuni <yoonh12288@gmail.com>
2024-10-06 22:28:59 +0900

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -11,21 +11,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository; // 데이터베이스와 상호작용하는 리포지토리
+    private UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder; // 비밀번호 암호화
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public void signUp(User user) {
-        // 중복 아이디 검사
-        if (userRepository.existsByUsername(user.getUsername())) {
-            throw new IllegalArgumentException("아이디가 이미 존재합니다.");
-        }
-
-        // 비밀번호 암호화
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        // 사용자 저장
+    public void saveUser(User user) {
+        String username = user.getUsername();  // getUsername 메서드 호출
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());  // getPassword 메서드 호출
+        user.setPassword(encryptedPassword);
         userRepository.save(user);
     }
 }

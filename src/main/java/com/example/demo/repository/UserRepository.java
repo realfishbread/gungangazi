2072ea196b97a8378f645a140cb26e5ddbf3740a
yuni <yoonh12288@gmail.com>
2024-10-06 22:20:21 +0900

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
-
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {

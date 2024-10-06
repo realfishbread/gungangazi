015b8ce9f450a9fed37533b6196cd149e4894d4b
yuni <yoonh12288@gmail.com>
2024-10-06 22:11:02 +0900

    Update and rename UserRepository.java to userRepository.java

--- src/main/java/com/example/demo/repository/userRepository.java
+++ src/main/java/com/example/demo/repository/userRepository.java
@@ -2,6 +2,6 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface userRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username); // 사용자 아이디 중복 확인 메서드
 }

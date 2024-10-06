1eb548d14da0b371863e2b29e776c241fabe2ef6
yuni <yoonh12288@gmail.com>
2024-10-06 21:11:38 +0900

    Update Controller.java

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class DemoController {
+public class Controller {
 
     @GetMapping("/hello")
     public String hello() {

fc4167a73faaa606860ed9bbbeedd4246656c435
yuni <yoonh12288@gmail.com>
2024-10-07 23:09:08 +0900

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username); // 사용자 아이디 중복 확인 메서드
+    User findByUsername(String username);
 }

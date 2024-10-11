0f06298c548a6d20ebc672e85a2c15661cbafea5
yuni <yoonh12288@gmail.com>
2024-10-11 20:19:25 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,6 @@ class MyApp extends StatelessWidget {
         '/homeApp': (context) => const MobileHomePage(), // 앱 전용 페이지
         
       },
-      home: kIsWeb ? const WebHomePage() : const MobileHomePage(), // 웹 또는 앱 구분
-      debugShowCheckedModeBanner: false,
     );
   }
 

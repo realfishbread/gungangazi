a4156370756ce3461640edfc7bfd1b867bcbe439
realfishbread <yoonh12288@gmail.com>
2024-10-22 20:24:23 +0900

    zebal

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username); // 사용자 아이디 중복 확인 메서드
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }

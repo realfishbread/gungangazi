801b316bcf67410d9844b05ec5593de2b4810804
realfishbread <yoonh12288@gmail.com>
2024-10-22 20:44:21 +0900

    sibal

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -75,7 +75,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b19db820c636e0bf0e6214e78cea26c7
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "46deec5479fc170358e31f9be76cd4e4",
+"flutter_bootstrap.js": "61f685235afb27c0cf5d4c2bb18c6371",
 "index.html": "61425982e7c99869ff763f5346abb5a9",
 "/": "61425982e7c99869ff763f5346abb5a9",
 "main.dart.js": "e68d49c34cb0547e2087b22dbf0482e3",

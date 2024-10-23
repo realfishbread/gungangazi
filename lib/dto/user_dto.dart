0b09d32cdbc3267f3995c0a1d543f2954f4a734e
realfishbread <yoonh12288@gmail.com>
2024-10-23 22:28:17 +0900

    zebal...

--- lib/dto/user_dto.dart
+++ lib/dto/user_dto.dart
@@ -1,13 +1,13 @@
 // user_dto.dart
 class UserDTO {
-  final String name;
+  final String username;
   final String email;
   final String id;
   final String password;
   final String gender;
 
   UserDTO({
-    required this.name,
+    required this.username,
     required this.email,
     required this.id,
     required this.password,
@@ -17,7 +17,7 @@ class UserDTO {
   // DTO를 JSON으로 변환하는 메서드
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
+      'username': username,
       'email': email,
       'id': id,
       'password': password,

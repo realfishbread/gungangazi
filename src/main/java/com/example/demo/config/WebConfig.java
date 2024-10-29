0d0e23933515cd55cab28c2405f549d08206bdee
realfishbread <yoonh12288@gmail.com>
2024-10-29 17:59:12 +0900

    zebal

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // API 엔드포인트에 CORS 적용
                 .allowedOrigins(
                     "https://gungangazi.site",
+                    "https://gungangazi.site/#/splash",
                     "http://localhost:8080",   // 로컬 개발 환경 (백엔드)
                     "http://localhost:3000",
                     "http://localhost:56272"

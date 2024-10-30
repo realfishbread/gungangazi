e78e4a2d54e38194fe5c8235a1a6d123b75638fb
realfishbread <yoonh12288@gmail.com>
2024-10-30 20:06:14 +0900

    watashi

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -75,10 +75,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "b19db820c636e0bf0e6214e78cea26c7
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "754a787b2263282e981972138ea0264c",
+"flutter_bootstrap.js": "10c5a5e5508faa04319b93cec60855fa",
 "index.html": "61425982e7c99869ff763f5346abb5a9",
 "/": "61425982e7c99869ff763f5346abb5a9",
-"main.dart.js": "501f0b1ba17b05bde54cd56fa735c953",
+"main.dart.js": "6eedcb12fb85b447df2a2df6b231cc03",
 "version.json": "952e01e7a8ca09393941c7becdb8cc0a"};
 // The application shell files that are downloaded before a service worker can
 // start.

664552bf64dfbba88e4ac190eb6b7222cfd89b3c
realfishbread <yoonh12288@gmail.com>
2024-11-03 22:40:37 +0900

    uhhh

--- lib/services/TokenService.dart
+++ lib/services/TokenService.dart
@@ -1,41 +1,43 @@
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:jwt_decode/jwt_decode.dart'; // jwt_decode 패키지를 임포트
+import 'package:jwt_decode/jwt_decode.dart';
 
 class TokenService {
-  static const String _tokenKey = 'auth_token';  // 토큰 저장을 위한 키
+  static const String _tokenKey = 'auth_token';
 
   // 토큰 저장 함수
   Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_tokenKey, token);  // 토큰을 SharedPreferences에 저장
+    await prefs.setString(_tokenKey, token);
   }
 
   // 토큰 불러오는 함수
   Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString(_tokenKey);
-    print("토큰 값: $token"); // 토큰 값 출력
-    return token;
+    return prefs.getString(_tokenKey);
   }
 
   // 토큰 삭제 함수 (로그아웃 시 사용)
   Future<void> deleteToken() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_tokenKey);  // 토큰 삭제
+    await prefs.remove(_tokenKey);
   }
 
   // 토큰에서 username을 추출하는 함수
   Future<String?> getUsername() async {
-    String? token = await getToken(); // 토큰을 가져옵니다.
+    String? token = await getToken();
     if (token != null) {
       try {
-        Map<String, dynamic> payload = Jwt.parseJwt(token); // 토큰을 디코딩하여 페이로드를 가져옵니다.
-        return payload['username']; // 페이로드에서 username을 추출합니다.
+        Map<String, dynamic> payload = Jwt.parseJwt(token);
+        String? username = payload['sub']; // JWT에서 'sub' 클레임을 username으로 사용
+        if (username != null) {
+          return username;
+        } else {
+          print('username 필드를 찾을 수 없습니다.');
+        }
       } catch (e) {
         print('토큰 디코딩 중 에러 발생: $e');
-        return null;
       }
     }
-    return null; // 토큰이 없을 경우 null 반환
+    return null;
   }
 }

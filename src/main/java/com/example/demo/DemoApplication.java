e3875554f29d33567d226c069091d74a3450dcf0
realfishbread <yoonh12288@gmail.com>
2024-11-19 19:52:54 +0900

    Yee

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication  // Spring Boot 애플리케이션임을 나타내는 어노테이션
-@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {

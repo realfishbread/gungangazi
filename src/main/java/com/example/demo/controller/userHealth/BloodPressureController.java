dc3f2272f65634e41ced18feda4b647732e8109e
realfishbread <yoonh12288@gmail.com>
2024-11-27 19:58:35 +0900

    Yee

--- src/main/java/com/example/demo/controller/userHealth/BloodPressureController.java
+++ src/main/java/com/example/demo/controller/userHealth/BloodPressureController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.userHealth;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,5 +31,11 @@ public class BloodPressureController {
     @GetMapping("/getBloodPressureData")
     public List<BloodPressure> getBloodPressureDataByUsername(@RequestParam String username) {
         return bloodPressureService.getBloodPressureDataByUsername(username);
+    }
+
+    @DeleteMapping("/deleteBloodPressureData")
+    public String deleteBloodPressureData(@RequestParam Long id) {
+        bloodPressureService.deleteBloodPressureDataById(id);
+        return "혈압 데이터가 삭제되었습니다.";
     }
 }

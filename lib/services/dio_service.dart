512581fcba6191432a507a6cd90b95caf7c94d2f
realfishbread <yoonh12288@gmail.com>
2024-12-20 01:15:51 +0900

    yee

--- lib/services/dio_service.dart
+++ lib/services/dio_service.dart
@@ -12,8 +12,8 @@ class DioService {
   Dio getDio() {
     Dio dio = Dio(BaseOptions(
       baseUrl: baseUrl,
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 3),
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
     ));
 
     dio.interceptors.add(InterceptorsWrapper(

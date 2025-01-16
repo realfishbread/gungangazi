603d51450c6d70990fc8fc69b0c44d4db4d1ad87
realfishbread <yoonh12288@gmail.com>
2025-01-16 22:10:48 +0900

    zebal google plz

--- lib/loginPge.dart
+++ lib/loginPge.dart
@@ -14,7 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 final GoogleSignIn _googleSignIn = GoogleSignIn(
-  clientId: '423735826070-9dq9dd52a4t5u66krjlg2nm0cpq8f92o.apps.googleusercontent.com.apps.googleusercontent.com',
+  clientId: '423735826070-9dq9dd52a4t5u66krjlg2nm0cpq8f92o.apps.googleusercontent.com.',
 );
 
 class _LoginPageState extends State<LoginPage> {

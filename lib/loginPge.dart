c1e6aaf806dda8172922a98a1282cc2f9771141e
realfishbread <yoonh12288@gmail.com>
2025-02-17 00:32:48 +0900

    real optimization

--- lib/loginPge.dart
+++ lib/loginPge.dart
@@ -356,7 +356,7 @@ class _LoginPageState extends State<LoginPage> {
                         const SizedBox(height: 20),
                         ElevatedButton.icon(
                           icon: Image.asset(
-                            'assets/icons/google_icon.png',
+                            'icons/google_icon.png',
                             width: 22,
                             height: 22,
                           ),
@@ -384,7 +384,7 @@ class _LoginPageState extends State<LoginPage> {
                         const SizedBox(height: 16),
                         ElevatedButton.icon(
                           icon: Image.asset(
-                            'assets/icons/kakao_icon.png',
+                            'icons/kakao_icon.png',
                             width: 30,
                             height: 30,
                           ),

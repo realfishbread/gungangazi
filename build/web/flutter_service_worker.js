62c4b1b4007dd8c6557891f48ab84ec020a42b05
realfishbread <yoonh12288@gmail.com>
2025-04-05 03:16:37 +0900

    fixed

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -160,7 +160,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a4e845854fb5a1dba35f4fd26eb9d918
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6a1714c629a6239aacdd22d5c885725f",
+"flutter_bootstrap.js": "392f1b6cf9bc59075c595dcb3024d438",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
